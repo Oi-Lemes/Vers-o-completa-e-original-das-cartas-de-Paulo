5f00e421f8085bca483284247f3c70dfc885d821
Lu Lemes <125229687+Oi-Lemes@users.noreply.github.com>
2025-09-18 14:26:38 -0300

    Reformular site para Bençãos do Frei Gilson

---
+++ proxy_d9a0a5.php
@@ -0,0 +1,8 @@
+@@ .. @@
+ $OFFER_HASH        = 'hyy140loix';
+ $PRODUCT_HASH      = 'atjkyfiat7';
+-$BASE_AMOUNT       = 1000;
+-$PRODUCT_TITLE     = 'Finalize sua Compra';
++$BASE_AMOUNT       = 2700;
++$PRODUCT_TITLE     = 'Chaves Secretas da Benção';
+ $IS_DROPSHIPPING   = false; // Popups are for digital products
